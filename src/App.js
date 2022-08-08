47d94a8889aa3727589d8e914042ae76b086f3e5
d0422 <rlfehd2013@naver.com>
2022-08-08 21:32:56 +0900

    :zap: styled-compponents

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import styled from "styled-components";
+const Father = styled.div`
+  display: flex;
+`;
+const Box = styled.div`
+  background-color: ${(props) => props.color};
+  width: 100px;
+  height: 100px;
+`;
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+const Text = styled.span`
+  color: white;
+`;
+const Input = styled.input.attrs({ required: true })``;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father>
+      <Box color="teal">
+        <Text>어쩔티비</Text>
+      </Box>
+      <Circle color="tomato"></Circle>
+      <Input></Input>
+    </Father>
   );
 }
 
