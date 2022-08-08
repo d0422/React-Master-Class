753a8155fa2b502dd47b612a553cd63c2c1309f3
d0422 <rlfehd2013@naver.com>
2022-08-08 22:28:09 +0900

    :zap: styled-components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
+const animation = keyframes`
+  0%{
+    transform :rotate(0deg);
+    border-radius: 0px;
+  }
+  50%{
+    transform:rotate(360deg);
+    border-radius:100px;
+  }
+  100%{
+    transform :rotate(0deg);
+    border-radius: 0px;
+  }
+`;
+const Text = styled.span`
+  color: ${(props) => props.theme.textColor};
+`;
 const Box = styled.div`
   background-color: ${(props) => props.color};
-  width: 100px;
-  height: 100px;
+  width: 200px;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  animation: ${animation} 5s linear infinite;
+  ${Text} {
+    font-size: 20px;
+    &:hover {
+      font-size: 80px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 const Circle = styled(Box)`
   border-radius: 50px;
-`;
-const Text = styled.span`
-  color: white;
 `;
 const Input = styled.input.attrs({ required: true })``;
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "styled-components";
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+const lightTheme = {
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
