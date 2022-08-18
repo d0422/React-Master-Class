015833941f84b58d2e5f7e0740493423df0fdeec
d0422 <rlfehd2013@naver.com>
2022-08-18 17:49:37 +0900

    :zap: framer-motion

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
 `;
 
@@ -15,15 +16,98 @@ const Box = styled(motion.div)`
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
-
+const MiniBox = styled(motion.div)`
+  width: 50px;
+  height: 50px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+const CBox = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  place-self: center;
+`;
+const myVar = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+};
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.3,
+      staggerChildren: 0.1,
+    },
+  },
+};
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
+const BiggerBox = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 0.3);
+  border-radius: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+`;
+const interactVariants = {
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px" },
+};
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-        transition={{ type: "spring", delay: 0.5 }}
-      />
+      <Box variants={myVar} initial="start" animate="end" />
+      <CBox variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants}></Circle>
+        <Circle variants={circleVariants}></Circle>
+        <Circle variants={circleVariants}></Circle>
+        <Circle variants={circleVariants}></Circle>
+      </CBox>
+      <BiggerBox ref={biggerBoxRef}>
+        <MiniBox
+          drag
+          dragSnapToOrigin
+          dragConstraints={biggerBoxRef}
+          dragElastic={0.2}
+          variants={interactVariants}
+          whileHover={"hover"}
+          whileTap={"click"}
+          whileDrag={{ backgroundColor: "rgb(153,175,255)" }}
+        ></MiniBox>
+      </BiggerBox>
     </Wrapper>
   );
 }
--- animation/src/index.tsx
+++ animation/src/index.tsx
@@ -64,7 +64,7 @@ body {
   font-family: 'Source Sans Pro', sans-serif;
   color:black;
   line-height: 1.2;
-  background:linear-gradient(135deg,#e09,#d0e);
+  background:linear-gradient(90deg,rgb(153,175,255),rgb(168,204,255));
 }
 a {
   text-decoration:none;
