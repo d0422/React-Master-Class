963df14168bddc4fefdea4f902a08c034f105a98
d0422 <rlfehd2013@naver.com>
2022-08-24 14:58:38 +0900

    :zap: 0824

---
+++ trello-clone/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <input type="number" placeholder="Minutes"></input>
+      <input type="number" placeholder="Time"></input>
+    </>
+  );
+}
+
+export default App;
