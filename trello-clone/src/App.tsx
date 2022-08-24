eb0184b5ca313ff2e8e392624f7b67fa32527fa8
d0422 <rlfehd2013@naver.com>
2022-08-24 15:42:43 +0900

    :zap: setselector

--- trello-clone/src/App.tsx
+++ trello-clone/src/App.tsx
@@ -1,10 +1,31 @@
 import React from "react";
-
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
+import { Draggable, DragDropContext, Droppable } from "@hello-pangea/dnd";
 function App() {
+  const [minute, setMinute] = useRecoilState(minuteState);
+  const [hour, setHour] = useRecoilState(hourSelector);
+  const 분바꾸기 = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinute(+event.currentTarget.value);
+  };
+  const 시간바꾸기 = (event: React.FormEvent<HTMLInputElement>) => {
+    setHour(+event.currentTarget.value);
+  };
+
   return (
     <>
-      <input type="number" placeholder="Minutes"></input>
-      <input type="number" placeholder="Time"></input>
+      <input
+        type="number"
+        value={minute}
+        onChange={분바꾸기}
+        placeholder="Minutes"
+      ></input>
+      <input
+        type="number"
+        placeholder="Time"
+        value={hour}
+        onChange={시간바꾸기}
+      ></input>
     </>
   );
 }
